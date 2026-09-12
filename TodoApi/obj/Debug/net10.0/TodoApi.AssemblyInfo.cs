a6062a55e81f13c1f07e22393b08754c41d807d4
preechaporn jeksoongnoen <preechapjorn.j@ku.th>
2026-09-12 16:40:52 +0700

    merge from get-todo-endpoint

--- TodoApi/obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3588a2bd0eea32b2798e85db4461e754d57dfe9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32fd075e6aa59764a1c6d80ad0ad57a60b74114d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
