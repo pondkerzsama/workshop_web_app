543b671922f5c97a97522a74814fdd66deb70c6f
preechaporn jeksoongnoen <preechapjorn.j@ku.th>
2026-09-12 15:27:03 +0700

    chance weather api to hello api

--- TodoApi/obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b929b2b1504aa60e743a8f52d137126239baf082")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3588a2bd0eea32b2798e85db4461e754d57dfe9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
