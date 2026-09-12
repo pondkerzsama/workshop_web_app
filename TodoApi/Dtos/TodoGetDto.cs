686e4f4a5314907c85c91a2dd447a0161668f3cc
preechaporn jeksoongnoen <preechapjorn.j@ku.th>
2026-09-12 16:36:26 +0700

    feat : implement api method Get

--- TodoApi/Dtos/TodoGetDto.cs
+++ TodoApi/Dtos/TodoGetDto.cs
@@ -1,4 +1,4 @@
-namespace TodoApi;
+namespace TodoApi.Dtos;
 
 public record TodoGetDto
 (
--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,3 +1,4 @@
+using TodoApi.Dtos;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,15 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+var todos = new List<TodoGetDto>
+{
+    new (1, "string", true),
+    new (2, "smt", false),
+    new (3, "smt2", true)
+};
+
+app.MapGet("/todos", () => Results.Ok(todos));
 
 app.MapGet("/", () => "Hello Todo API");
 
