0a389d23ed743af024ffcb6efaacdb54f08db7ed
preechaporn jeksoongnoen <preechapjorn.j@ku.th>
2026-09-13 18:37:04 +0700

    ..

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -46,6 +47,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
